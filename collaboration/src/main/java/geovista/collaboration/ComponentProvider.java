492a445239d2f20bf177533c9890ee39af28d983
Frank Hardisty <frank.hardisty@gmail.com>
2014-11-24 16:20:16 -0500

    adding ComponentProvider

---
+++ collaboration/src/main/java/geovista/collaboration/ComponentProvider.java
@@ -0,0 +1,13 @@
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
+
+package geovista.collaboration;
+
+public interface ComponentProvider {
+
+	public String marshalCurrentComponent();
+
+	public boolean componentProviderExists();
+
+}
